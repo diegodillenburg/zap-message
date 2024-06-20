e084de38437c58878b6b59efa0459c720bb59ac9
Diego Dillenburg <diegodillenburg+github@gmail.com>
2024-06-19 23:29:19 -0300

    Adds ImageMessage

--- lib/zap_message/model.rb
+++ lib/zap_message/model.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'zap_message/model/message'
 require 'zap_message/model/interactive_list_message'
+require 'zap_message/model/image_message'
 require 'zap_message/model/text_message'
 require 'zap_message/model/read_receipt_message'
---
+++ lib/zap_message/model/image_message.rb
@@ -0,0 +1,42 @@
+require 'zap_message/model/message'
+
+module ZapMessage
+  module Model
+    class ImageMessage < Message
+      EMPTY_ATTRIBUTES = {}.freeze
+      # TODO: add constraints
+      # (id | link) required
+      # extension.in?(jpeg, png)
+      # size <= 5mb
+
+      attr_accessor :id, :link, :caption
+
+      def initialize(**attrs)
+        super(**attrs)
+        @type ||= 'image'
+      end
+
+      private
+
+      def message_type_attributes
+        {
+          image: {}.merge(media_attributes).merge(caption_attributes)
+        }
+      end
+
+      def media_attributes
+        if id
+          { id: id }
+        else
+          { link: link }
+        end
+      end
+
+      def caption_attributes
+        return EMPTY_ATTRIBUTES unless caption
+
+        { caption: caption }
+      end
+    end
+  end
+end
