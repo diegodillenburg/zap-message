f457cadc2b2688d86f34102414796991a842034c
Diego Dillenburg <diegodillenburg+github@gmail.com>
2024-06-20 19:05:19 -0300

    Adds DocumentMessage

---
+++ lib/zap_message/model/document_message.rb
@@ -0,0 +1,43 @@
+require 'zap_message/model/message'
+
+module ZapMessage
+  module Model
+    class DocumentMessage < Message
+
+      attr_accessor :id, :link, :caption, :filename
+
+      def initialize(**attrs)
+        super(**attrs)
+        @type ||= 'document'
+      end
+
+      private
+
+      def message_type_attributes
+        {
+          document: media_attributes.merge(caption_attributes).merge(filename_attributes)
+        }
+      end
+
+      def media_attributes
+        if id
+          { id: id }
+        else
+          { link: link }
+        end
+      end
+
+      def caption_attributes
+        return EMPTY_ATTRIBUTES unless caption
+
+        { caption: caption }
+      end
+
+      def filename_attributes
+        return EMPTY_ATTRIBUTES unless filename
+
+        { filename: filename }
+      end
+    end
+  end
+end
