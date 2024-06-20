87920038da55f53e51b013e724471394ead54fbe
Diego Dillenburg <diegodillenburg+github@gmail.com>
2024-06-20 20:15:30 -0300

    Adds ReactionMessage

---
+++ lib/zap_message/model/reaction_message.rb
@@ -0,0 +1,26 @@
+require 'zap_message/model/message'
+
+module ZapMessage
+  module Model
+    class ReactionMessage < Message
+
+      attr_accessor :message_id, :emoji
+
+      def initialize(**attrs)
+        super(**attrs)
+        @type ||= 'reaction'
+      end
+
+      private
+
+      def message_type_attributes
+        {
+          reaction: {
+            message_id: message_id,
+            emoji: emoji
+          }
+        }
+      end
+    end
+  end
+end
